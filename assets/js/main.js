b25ec8424d2bd0cef497c565ae484381e1cc821b
asdfjennifer <jennifergenger@gmailcom>
2022-05-29 22:27:19 +0200

    ich hab's versucht :(

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+//Übung lev2_1
+
+let edelMetallPreise = [
+    { name: "Gold", preiseGramEuro: 42.91, veraenderung: "+0.12%" },
+    { name: "Silber", preiseGramEuro: 0.51, veraenderung: "+1.02%" },
+    { name: "Platin", preiseGramEuro: 25.74, veraenderung: "+0.41%" },
+    { name: "Palladium", preiseGramEuro: 50.93, veraenderung: "0.00%" },
+    { name: "Rhodium", preiseGramEuro: 160.12, veraenderung: "-0.10%" },
+    { name: "Iridium", preiseGramEuro: 42.84, veraenderung: "0.00%" },
+    { name: "Ruthenium", preiseGramEuro: 7.36, veraenderung: "0.00%" },
+    { name: "Rhenium", preiseGramEuro: 37.22, veraenderung: "-0.53%" },
+    { name: "Osmium", preiseGramEuro: 11.54, veraenderung: "0.00%" }
+];
+
+// Greife auf die Objekte im Array mit Hilfe von map(), forEach(), filter() zu.
+// Der Code dazu ist im Kommentarbereich.
+// Verwende forEach() und greife auf alle name zu. Pushe diese in ein neues Array.
+// Verwende map() und greife auf alle name zu.
+// Verwende forEach() und greife auf alle preiseGramEuro zu und pushe diese ein neues Array.
+// Verwende map() und greife auf alle preiseGramEuro zu.
+// Verwende forEach() und greife auf alle veraenderung zu und pushe in ein neues Array.
+// Verwende map() und greife auf alle veraenderung zu.
+// Verwende filter() und greife auf preiseGramEuro die teurer als 50 Euro zu 
+// Gib alles als Tabelle in deinem HTML-Dokument aus.
+
+let name = () => {
+    console.log(name.forEach());
+}
+
+
+
+
+//Übung lev2_2
+
+const singers = [
+	{ name: 'The Beatles', country: 'United Kingdom', period_active: { start: 1960, end: 1970 }, genre: "Rock / Pop" },
+	{ name: 'Elvis Presley', country: 'United States', period_active: { start: 1954, end: 1977 }, genre: "Rock and roll" },
+	{ name: 'Michael Jackson', country: 'United States', period_active: { start: 1964, end: 2009 }, genre: "Pop / Rock / Dance / Soul / R&B"},
+	{ name: 'Elton John', country: 'United Kingdom', period_active: { start: 1964, end: "present" }, genre: "Pop / Rock"},
+	{ name: 'Madonna', country: 'United States', period_active: { start: 1979, end: "present" }, genre: "Pop / Dance / Electronica"},
+	{ name: 'Led Zeppelin', country: 'United Kingdom', period_active: { start: 1968, end: 1980 }, genre: "Hard rock / Blues rock / Folk rock"},
+	{ name: 'Rihanna', country: 'United States', period_active: { start: 2005, end: "present" }, genre: "R&B / Pop / Dance / Hip-hop"},
+	{ name: 'Pink Floyd', country: 'United Kingdom', period_active: { start: 1965, end: 1996, extra: 2014 }, genre: "Progressive rock / Psychedelic rock"},
+];
+
+// let sort = () => {
+//     if () {
+//         return ;
+//     }
+//     console.log(NewArray);
+// }
